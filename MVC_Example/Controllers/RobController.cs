63eec757c398c3b20c5887b4c7d63028a10763d8
Robert Robinson <robinson6113@student.cptc.edu>
2019-01-08 11:15:21 -0800

    Add project files.

---
+++ MVC_Example/Controllers/RobController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Example.Controllers
+{
+    public class RobController : Controller
+    {
+        public IActionResult Bio()
+        {
+            return View();
+        }
+    }
+}
